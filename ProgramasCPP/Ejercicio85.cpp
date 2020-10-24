2730c0842a6b382af75bc517a391a3263f9cd485
Rodriguez Martinez <47833756+EmmanuelARodriguez@users.noreply.github.com>
2020-10-23 21:11:10 -0700

    Update Ejercicio85.cpp

--- ProgramasCPP/Ejercicio85.cpp
+++ ProgramasCPP/Ejercicio85.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 int main () {
-
-      doble x1 = 0, y1 = 10, x2 = -3, y2 = 0;
-			doble m;
-			cout << "x1:" << x1 << "\ n";
-			cout << "y1:" << y1 << "\ n";
-			cout << "x2:" << x2 << "\ n";
-			cout << "y2:" << y2 << "\ n";
-			m = (y2 - y1) / (x2 - x1); 
-			cout << "La pendiente es:" << m << endl;
+	
+	float x1 = 0, y1 = 10, x2 = -3, y2 = 0;
+	float m;
+	cout << "x1:" << x1 << "\n";
+	cout << "y1:" << y1 << "\n";
+	cout << "x2:" << x2 << "\n";
+	cout << "y2:" << y2 << "\n";
+	m = (y2 - y1) / (x2 - x1); 
+	cout << "La pendiente es:" << m << endl;
     return 0;
 }
