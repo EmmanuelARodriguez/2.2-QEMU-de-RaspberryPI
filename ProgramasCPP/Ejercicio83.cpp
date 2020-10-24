a01d20dc8b87c6de503e846df578a6921cefe4c0
Rodriguez Martinez <47833756+EmmanuelARodriguez@users.noreply.github.com>
2020-10-23 21:05:05 -0700

    Update Ejercicio83.cpp

--- ProgramasCPP/Ejercicio83.cpp
+++ ProgramasCPP/Ejercicio83.cpp
@@ -5,14 +5,13 @@
 using namespace std;
 
 int main () {
-
-      doble x1 = 56, y1 = 16, x2 = -40, y2 = 1;
-			doble m;
-			cout << "x1:" << x1 << "\ n";
-			cout << "y1:" << y1 << "\ n";
-			cout << "x2:" << x2 << "\ n";
-			cout << "y2:" << y2 << "\ n";
-			m = (y2 - y1) / (x2 - x1);
-			cout << "La ecuación es:" << m << "X - Y +" << ((m * x1) + y1) << endl;
+	float x1 = 56, y1 = 16, x2 = -40, y2 = 1;
+	float m;
+	cout << "x1:" << x1 << "\n";
+	cout << "y1:" << y1 << "\n";
+	cout << "x2:" << x2 << "\n";
+	cout << "y2:" << y2 << "\n";
+	m = (y2 - y1) / (x2 - x1);
+	cout << "La ecuación es: " << m << "X - Y +" << ((m * x1) + y1) << endl;
     return 0;
 }
