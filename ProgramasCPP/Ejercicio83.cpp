a25f931a637166ddb23e98185264ba1d775df0aa
Rodriguez Martinez <47833756+EmmanuelARodriguez@users.noreply.github.com>
2020-10-23 20:40:15 -0700

    Update Ejercicio83.cpp

--- ProgramasCPP/Ejercicio83.cpp
+++ ProgramasCPP/Ejercicio83.cpp
@@ -2,16 +2,17 @@
 // Determinar la ecuación de la recta que pasa por los puntos (56,16) y (-40,1).
 
 #include <iostream>
+using namespace std;
 
 int main () {
 
       doble x1 = 56, y1 = 16, x2 = -40, y2 = 1;
 			doble m;
-			std :: cout << "x1:" << x1 << "\ n";
-			std :: cout << "y1:" << y1 << "\ n";
-			std :: cout << "x2:" << x2 << "\ n";
-			std :: cout << "y2:" << y2 << "\ n";
+			cout << "x1:" << x1 << "\ n";
+			cout << "y1:" << y1 << "\ n";
+			cout << "x2:" << x2 << "\ n";
+			cout << "y2:" << y2 << "\ n";
 			m = (y2 - y1) / (x2 - x1);
-			std :: cout << "La ecuación es:" << m << "X - Y +" << ((m * x1) + y1) << std :: endl;
+			cout << "La ecuación es:" << m << "X - Y +" << ((m * x1) + y1) << endl;
     return 0;
 }
