68f66adf883d6b54d473ef15d08163b35bd0d860
Rodriguez Martinez <47833756+EmmanuelARodriguez@users.noreply.github.com>
2020-10-23 20:38:31 -0700

    Update Ejercicio40.cpp

--- ProgramasCPP/Ejercicio40.cpp
+++ ProgramasCPP/Ejercicio40.cpp
@@ -2,15 +2,16 @@
 // Calcular el area superficial de S de un prisma con dimensiones l, h, w. En este problema, l = 10, h = 4 yw = 5.2m.
 
 #include <iostream>
+using namespace std;
 
 int main () {
 
       doble l = 10, h = 4, w = 5.2;
 			doble m;
-			std :: cout << "l (largo):" << l << "\ n";
-			std :: cout << "h (alto):" << h << "\ n";
-			std :: cout << "w (ancho):" << w << "\ n";
+			cout << "l (largo):" << l << "\ n";
+			cout << "h (alto):" << h << "\ n";
+			cout << "w (ancho):" << w << "\ n";
 			S = l * h * w;
-			std :: cout << "El area superficial es de:" << S << "m." << std :: endl;
+			cout << "El area superficial es de:" << S << "m." << endl;
     return 0;
 }
