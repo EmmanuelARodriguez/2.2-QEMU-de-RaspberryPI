dcd9643e14acf160d5ffc60790a5efa21aea78ff
Rodriguez Martinez <47833756+EmmanuelARodriguez@users.noreply.github.com>
2020-10-23 20:40:58 -0700

    Update Ejercicio84.cpp

--- ProgramasCPP/Ejercicio84.cpp
+++ ProgramasCPP/Ejercicio84.cpp
@@ -2,14 +2,15 @@
 // Determinar la ecuación de la recta descrita por la pendiente 3 y el punto sobre la recta (8, -4)
 
 #include <iostream>
+using namespace std;
 
 int main () {
 
       doble x1 = 8, y1 = -4;
 			doble m = 3;
-			std :: cout << "x1:" << x1 << "\ n";
-			std :: cout << "y1:" << y1 << "\ n";
-			std :: cout << "pendiente:" << m << "\ n";
-			std :: cout << "La ecuación es:" << m << "X - Y +" << ((m * x1) + y1) << std :: endl;
+			cout << "x1:" << x1 << "\ n";
+			cout << "y1:" << y1 << "\ n";
+			cout << "pendiente:" << m << "\ n";
+			cout << "La ecuación es:" << m << "X - Y +" << ((m * x1) + y1) << endl;
     return 0;
 }
