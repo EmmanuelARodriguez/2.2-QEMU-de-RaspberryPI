62439f0e255019acd46ad5f2b04e0f1d00e5478a
Rodriguez Martinez <47833756+EmmanuelARodriguez@users.noreply.github.com>
2020-10-23 20:41:44 -0700

    Update Ejercicio85.cpp

--- ProgramasCPP/Ejercicio85.cpp
+++ ProgramasCPP/Ejercicio85.cpp
@@ -2,16 +2,17 @@
 // Imprimir la pendiente de la recta con una intercepcion y de (0,10) y un punto (-3,0).
 
 #include <iostream>
+using namespace std;
 
 int main () {
 
       doble x1 = 0, y1 = 10, x2 = -3, y2 = 0;
 			doble m;
-			std :: cout << "x1:" << x1 << "\ n";
-			std :: cout << "y1:" << y1 << "\ n";
-			std :: cout << "x2:" << x2 << "\ n";
-			std :: cout << "y2:" << y2 << "\ n";
+			cout << "x1:" << x1 << "\ n";
+			cout << "y1:" << y1 << "\ n";
+			cout << "x2:" << x2 << "\ n";
+			cout << "y2:" << y2 << "\ n";
 			m = (y2 - y1) / (x2 - x1); 
-			std :: cout << "La pendiente es:" << m << std :: endl;
+			cout << "La pendiente es:" << m << endl;
     return 0;
 }
